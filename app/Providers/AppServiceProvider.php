baed1ce11f308a237992a80234e4d1ca5cb6d2f2
Farhan <146110996+HanNewbie@users.noreply.github.com>
2025-08-20 11:06:36 +0700

    BLUD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
     //    URL::forceScheme('https');
+    Carbon::setLocale('id');
+    \Illuminate\Support\Facades\App::setLocale('id');
     }
 }
